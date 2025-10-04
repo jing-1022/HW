60645fc9933240f477c5c9b6344edcf8cda3a563
jing-1022 <youjing953@gmai.com>
2025-10-04 14:40:06 +0800

    1ver

---
+++ divide.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    int num1 = 9;
+    int num2 = 10;
+    int result1 = num1 / 3;
+    int result2 = num2 % 3;
+  
+    printf(" 9除以3等於%d\n", result1);
+    printf(" 10除以3等於%d\n", result2);
+    return 0;
+}
