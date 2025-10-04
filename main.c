6ca0b5a1b0f46c563ba1bd606e86fbd56f1caf9c
jing-1022 <youjing953@gmai.com>
2025-10-04 12:30:13 +0800

    init

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// int main(void) {
+//     int *a = NULL;
+//     a = (int*)malloc(9*sizeof(int));
+//     for(int i = 0 ; i < 9; i++){
+//         a[i] = i+1;
+//     }
+//     for(int i = 0 ; i < 9; i++){
+//         printf("%d\n", a[i]);
+//     }
+//     if(a[0] == 0){
+//         printf("a[0] = 0\n");
+//     }
+//     free(a);
+//     return 0;
+// }
+
+// int main(void) {
+//     for (int i = 0; i < 2; i++) {
+//         srand(time(NULL));
+//         int player;
+//         printf("剪刀(0)、石頭(1)、布(2)，選一個 ");
+//         scanf("%d",&player);
+//         int computer;
+//         computer = rand() % 3;
+//         printf("電腦出的是%d\n", computer);
+//         if(player == 0 && computer == 2 || player == 1 && computer == 0 || player == 2 && computer == 1){
+//             printf("你贏\n");}
+//         else if(player == 0 && computer == 1 || player == 1 && computer == 2 || player == 2 && computer == 0){
+//             printf("你輸\n");}
+//         else if(player == computer){
+//             printf("幹嘛學我\n");}}
+//         return 0;
+
+// }
